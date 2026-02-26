5dda7fed9f34472d6437bd63110af96fc2384b42
striumas-tech <karminamth@gmail.com>
2026-02-26 15:13:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import re
 from datetime import datetime, timedelta, timezone
 from typing import Dict
 
-import nextcord as discord
+import discord
 from discord import app_commands
 from discord.ext import tasks
 from dotenv import load_dotenv
