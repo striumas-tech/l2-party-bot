d9c018476a158cbdfb67c0e4d1f59a4f55f0842f
striumas-tech <karminamth@gmail.com>
2026-02-26 16:26:20 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -307,4 +307,31 @@ async def leave(ctx):
     await update_party_message(party)
     await ctx.send("You left the party.")
 
+@bot.command()
+async def close(ctx):
+    if ctx.author.id not in user_party_map:
+        await ctx.send("You are not in a party.")
+        return
+
+    party_id = user_party_map[ctx.author.id]
+    party = active_parties.get(party_id)
+
+    if not party or ctx.author.id != party["leader_id"]:
+        await ctx.send("Only leader can close the party.")
+        return
+
+    del active_parties[party_id]
+    for uid in list(user_party_map):
+        if user_party_map[uid] == party_id:
+            del user_party_map[uid]
+
+    channel = bot.get_channel(party["channel_id"])
+    try:
+        message = await channel.fetch_message(party["message_id"])
+        await message.delete()
+    except:
+        pass
+
+    await ctx.send("Party closed.")
+
 bot.run(TOKEN)
