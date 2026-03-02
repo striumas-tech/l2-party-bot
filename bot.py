45e07c70a56a18cbd2f658ad3643457155171cd8
striumas-tech <karminamth@gmail.com>
2026-03-02 10:10:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -373,21 +373,16 @@ async def lfp(
     }
 
     active_parties[party_id] = party
-    user_party_map[interaction.user.id] = party_id
+user_party_map[interaction.user.id] = party_id
 
-    await interaction.response.send_message(
-        embed=build_embed(party),
-        view=PartyView(party_id)
-
-        sent = await interaction.original_response()
-        party["message_id"] = sent.id
-        party["reminded"] = False
+await interaction.response.send_message(
+    embed=build_embed(party),
+    view=PartyView(party_id)
 )
 
 sent = await interaction.original_response()
 party["message_id"] = sent.id
 party["reminded"] = False
-    )
 
 
 @tree.command(
